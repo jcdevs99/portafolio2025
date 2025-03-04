2cd7430b0437e6c401181bd6fa386c7b21796a56
jcdevs99 <jdxevs@gmail.com>
2025-03-03 22:08:49 -0500

    añadi dos imagenes y cambie el color secundario

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,29 @@
 import './index.css'
 import mifoto from '/src/assets/mifoto2.jpg'
+import tienda from '/src/assets/tienda.svg'
+import portafolioweb from '/src/assets/portafolioweb.svg'
+
 const Home = () => {
     return (
         <>
             <section className="container">
                 <img src={mifoto} alt="desarrollador web" className='miFotografia'/>
-                <h1>Hola quieres desarrollar algo?</h1>
+                <h1>Hola, soy John Córdova y desarrollo pagínas web que se ajusten a tus requerimientos empresariales y emprendimientos</h1>
+            </section>
+            <h2>¿Qué quieres que realizar hoy?</h2>
+            <section className='containerPlans'>
+                <article className="webPlans">
+                    <div className="">
+                        <img src={tienda} alt=""/>
+                    </div>
+                    <div className=""></div>
+                </article>
+                <article className="webPlans">
+                    <div className="">
+                        <img src={portafolioweb} alt=""/>
+                    </div>
+                    <div className=""></div>
+                </article>
             </section>
         </>
     )

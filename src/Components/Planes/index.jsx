e87d328b25ed3d63b1f8b31fbd51a3d9dc508b10
jcdevs99 <jdxevs@gmail.com>
2025-03-06 22:20:08 -0500

    añadi los planes en un componente separado

--- src/Components/Planes/index.jsx
+++ src/Components/Planes/index.jsx
@@ -8,43 +8,49 @@ const Planes = () => {
             <img src={tienda} alt=""/>
             <div className="descriptionPlan">
                 <h3 className='title'>Tienda online</h3>
-                <div className='description'>
-                    <p>Coloca y vende tus productos por internet las 24 horas del día, los 365 días del año.</p>
-                    <p>Ajusta costos de envio, añade impuestos, presenta tu producto estrella y ten preparado una landing page para promociones por temporada.</p>
-                </div>
-                <div>
-                    <details>
-                        <summary>¿Qué incluye?</summary>
-                        <ul className='details'>
-                            <li>Dominio <strong>www.tunegocio.com</strong></li>
-                            <li>Hosting 10gb</li>
-                            <li>Wordpress</li>
-                            <li>Certificado SSL</li>
-                            <li>Email empresarial <strong>support@tunegocio.com</strong></li>
-                            <li>Woocomerce para recibir pagos</li>
-                            <li>Protección DDoS</li>
-                            <li>Panel de control</li>
-                            <li>Ancho de banda ilimitado</li>
-                        </ul>
-                    </details>
-                </div>
+                <p>Coloca y vende tus productos por internet las 24 horas del día, los 365 días del año. Ajusta costos de envió, añade impuestos, presenta tu producto estrella y ten preparado una landing page para promociones por temporada.</p>
+                <details>
+                    <summary>¿Qué incluye?</summary>
+                    <ul className='details'>
+                        <li>Dominio <strong>www.tunegocio.com</strong></li>
+                        <li>Hosting 10gb</li>
+                        <li>Wordpress</li>
+                        <li>Certificado SSL</li>
+                        <li>Email empresarial <strong>support@tunegocio.com</strong></li>
+                        <li>Woocomerce para recibir pagos</li>
+                        <li>Protección DDoS</li>
+                        <li>Panel de control</li>
+                        <li>Ancho de banda ilimitado</li>
+                        <li>Soporte técnico</li>
+                        <li>Chat de WhatsApp</li>
+                    </ul>
+                </details>
             </div>
+            <button type='button'>Comprar</button>
         </article>
         <article className="webPlans">
             <img src={portafolioweb} alt=""/>
             <div className="descriptionPlan">
                 <h3 className='title'>Portafolio web</h3>
-                <div className='description'>
-                    <p>Coloca y vende tus productos por internet las 24 horas del día, los 365 días del año.</p>
-                    <p>Ajusta costos de envio, añade impuestos, presenta tu producto estrella y ten preparado una landing page para promociones por temporada.</p>
-                </div>
-                <div>
-                    <details>
-                        <summary>¿Qué incluye?</summary>
-                        <p className='details'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus explicabo, ipsam non alias optio voluptas itaque aliquam quod sed reprehenderit dolores tempore, rem suscipit cupiditate atque officia debitis repellendus perferendis.</p>
-                    </details>
-                </div>
+                <p>La primera imagen que reciben tus clientes es vital para generar confianza. Muestra lo que sabes hacer, a que te dedicas, establece confianza con tu cliente potencial y vende tus servicios.</p>
+                <details>
+                    <summary>¿Qué incluye?</summary>
+                    <ul className='details'>
+                        <li>Dominio <strong>www.tunegocio.com</strong></li>
+                        <li>Hosting 10gb</li>
+                        <li>Wordpress</li>
+                        <li>Certificado SSL</li>
+                        <li>Email empresarial <strong>support@tunegocio.com</strong></li>
+                        <li>Protección DDoS</li>
+                        <li>Panel de control</li>
+                        <li>Ancho de banda ilimitado</li>
+                        <li>Soporte técnico</li>
+                        <li>Chat de WhatsApp</li>
+                    </ul>
+                </details>
             </div>
+            <button type='button'>Comprar</button>
+
         </article>
     </section>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,9 +7,10 @@ const Home = () => {
         <>
             <section className="container">
                 <img src={mifoto} alt="desarrollador web" className='miFotografia'/>
-                <h1>Hola, soy John Córdova y desarrollo pagínas web que se ajusten a tus requerimientos empresariales y emprendimientos</h1>
+                <h1 style={{display:'none'}}> comprar una pagina web, crear una pagina web fácil, quiero desarrollar una web pero no se como, quiero comprar una pagina web para mi negocio o emprendimiento.</h1>
+                <p>Hola, soy John Córdova entusiasta Ecuatoriano me dedico a realizar páginas web para cualquier tipo de negocios, uso wordpress para el desarrollo de estas páginas. Por otro lado uso Javascript para el desarrollo de funcionalidades que requieran mayor complejidad.</p>
+                <p>¿Qué quieres realizar hoy?</p>
             </section>
-            <h2>¿Qué quieres realizar hoy?</h2>
             <Planes/>
         </>
     )
